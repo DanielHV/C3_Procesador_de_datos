eca3a90e8b8021cafac2097283bf0d7af4e8c1c3
Daniel Hernandez <daniel278@ciencias.unam.mx>
2025-09-25 17:34:22 -0600

    refactor(src/procesador/procesador): uso de función obtener_variables_regex_df importada
    De src/utils/regex_utils.py

--- src/procesador/procesador.py
+++ src/procesador/procesador.py
@@ -3,20 +3,7 @@ import numpy as np
 import pandas as pd
 pd.set_option('future.no_silent_downcasting', True)
 import re
-
-def obtener_variables_regex_df(df:pd.DataFrame, regex:str) -> list:
-    
-    if not isinstance(df, pd.DataFrame):
-        raise TypeError('El parámetro df debe ser de tipo pd.DataFrame')
-    
-    if not isinstance(regex, str):
-        raise TypeError('El parámetro regex debe ser de tipo str')
-    try:
-        pattern = re.compile(regex)
-    except re.error:
-        raise ValueError(f'La expresión regular {regex} no es válida')
-    
-    return list(df.filter(regex=regex).columns)
+from utils.regex_utils import obtener_variables_regex_df
 
 class Procesador:
 
